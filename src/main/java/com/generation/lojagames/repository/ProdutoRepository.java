726b15555b47cc3f0710ce528581214d2d828452
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-02 16:51:14 -0300

    Exercicio Loja de Games Bloco II

--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -1,5 +1,6 @@
 package com.generation.lojagames.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,10 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	//select * from tb_produtos nome
 	public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	public List<Produto> findAllByValorGreaterThanEqual(@Param("valor") BigDecimal valor);
+	public List<Produto> findAllByValorLessThanEqual(@Param("valor") BigDecimal valor);
+	public List<Produto> findAllByValorBetween(@Param("valor1") BigDecimal valor1, @Param("valor2") BigDecimal valor2);
+
+	
 
 }
