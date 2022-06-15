9e0614717d9ae813a4fd4afb23fb7f07e9d132f2
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-15 16:05:21 -0300

    Swagger Config add no Projeto

---
+++ src/main/java/com/generation/lojagames/configuration/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.generation.lojagames.configuration;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springBlogPessoalOpenAPI() {
+		return new OpenAPI().info(new Info().title("Projeto Lojas de Games")
+				.description("Loja de games - Projeto Generation Brasil 💖")
+				.version("v0.0.1")
+				.license(new License().name("Patrícia Souza").url("https://www.linkedin.com/in/patricia-souza-developerjava/"))
+				.contact(new Contact().name("GitHub 😻").url("https://github.com/PatriciAlves/")
+				.email("patricia.18_08@hotmail.com")));
+	}
+
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+
+	}
+
+}
